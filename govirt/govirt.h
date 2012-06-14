c56e45a93f3e38f1c53df2f6641a68d9fe1706a3
Christophe Fergeau <cfergeau@redhat.com>
2012-06-14 19:39:48 +0200

    Add govirt.h header
    This is just a convenience header including all the other
    installed headers.

---
+++ govirt/govirt.h
@@ -0,0 +1,28 @@
+/*
+ * govirt.h: main header
+ *
+ * Copyright (C) 2012 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+ *
+ * Author: Christophe Fergeau <cfergeau@redhat.com>
+ */
+#ifndef __OVIRT_H__
+#define __OVIRT_H__
+
+#include <govirt/ovirt-enum-types.h>
+#include <govirt/ovirt-rest-call.h>
+
+#endif /* __OVIRT_H__ */
