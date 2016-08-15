26565f76306736efd072a2c6a4546ec7aaf750bb
Christophe Fergeau <cfergeau@redhat.com>
2016-08-15 14:53:12 +0200

    Remove #ifdef HAVE_CONFIG_H
    Only one file was using it, and meson is not defining it.

--- govirt/ovirt-options.c
+++ govirt/ovirt-options.c
@@ -14,9 +14,7 @@
    You should have received a copy of the GNU Lesser General Public
    License along with this library; if not, see <http://www.gnu.org/licenses/>.
 */
-#ifdef HAVE_CONFIG_H
-# include "config.h"
-#endif
+#include <config.h>
 
 #include <glib.h>
 
