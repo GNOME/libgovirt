GNOME/libgovirt

commit 736b0e834becf0db56ec5ab3463a75535e7dd3ef

vm: Set vm state property using OvirtXmlElement struct
It was required to change the default value of the enum property to
OVIRT_VM_STATE_UNKNOWN, so that it will be set by
ovirt_rest_xml_node_parse() function in case of error.

Signed-off-by: Eduardo Lima (Etrunko) <etrunko@redhat.com>

govirt/ovirt-vm-xml.c
@@ -138,35 +138,7 @@ static gboolean vm_set_display_from_xml(OvirtVm *vm,
     return TRUE;
 }
 
-static gboolean vm_set_state_from_xml(OvirtVm *vm, RestXmlNode *node)
-{
-    RestXmlNode *state_node;
-
-    state_node = rest_xml_node_find(node, "status");
-    if (state_node == NULL) {
-        g_debug("Could not find 'status' node");
-        return FALSE;
-    }
-    state_node = rest_xml_node_find(state_node, "state");
-    if (state_node != NULL) {
-        int state;
-
-        g_return_val_if_fail(state_node->content != NULL, FALSE);
-        state = ovirt_utils_genum_get_value(OVIRT_TYPE_VM_STATE,
-                                            state_node->content,
-                                            OVIRT_VM_STATE_UNKNOWN);
-        g_object_set(G_OBJECT(vm), "state", state, NULL);
-
-        return TRUE;
-    }
-
-    return FALSE;
-}
-
 G_GNUC_INTERNAL gboolean ovirt_vm_refresh_from_xml(OvirtVm *vm, RestXmlNode *node)
 {
-    vm_set_state_from_xml(vm, node);
-    vm_set_display_from_xml(vm, node);
-
-    return TRUE;
+    return vm_set_display_from_xml(vm, node);
 }

govirt/ovirt-vm.c
@@ -199,6 +199,9 @@ static gboolean ovirt_vm_init_from_xml(OvirtResource *resource,
           .xml_path = "cluster",
           .xml_attr = "id",
         },
+        { .prop_name = "state",
+          .xml_path = "status/state",
+        },
         { NULL, },
     };
 
@@ -233,7 +236,7 @@ static void ovirt_vm_class_init(OvirtVmClass *klass)
                                                       "State",
                                                       "Virtual Machine State",
                                                       OVIRT_TYPE_VM_STATE,
-                                                      OVIRT_VM_STATE_DOWN,
+                                                      OVIRT_VM_STATE_UNKNOWN,
                                                       G_PARAM_READWRITE |
                                                       G_PARAM_STATIC_STRINGS));
     g_object_class_install_property(object_class,

commit 1e2fa2b0be434e4e840d36e781312bba5c8ce151

test-govirt: Add 'display' node to vm XMLs
Makes 'make distcheck' pass again. Test-govirt was failing since commit
736b0e8, because the 'display' node is mandatory, but the return value
of the function vm_set_display_from_xml() had been ignored.

Signed-off-by: Eduardo Lima (Etrunko) <etrunko@redhat.com>

tests/test-govirt.c
@@ -347,9 +347,17 @@ static void test_govirt_list_duplicate_vms(void)
     const char *vms_body = "<vms> \
                               <vm href=\"/ovirt-engine/api/vms/uuid0\" id=\"uuid0\"> \
                                 <name>vm0</name> \
+                                <display> \
+                                    <type>spice</type> \
+                                    <monitors>1</monitors> \
+                                </display> \
                               </vm> \
                               <vm href=\"/ovirt-engine/api/vms/uuid1\" id=\"uuid1\"> \
                                 <name>vm0</name> \
+                                <display> \
+                                    <type>spice</type> \
+                                    <monitors>1</monitors> \
+                                </display> \
                               </vm> \
                             </vms>";
 